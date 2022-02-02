2dc7398d728b09c4ab344778124d6f2eb74fa775
Carolina Marugan Rubio <54485798+codecamaru@users.noreply.github.com>
2022-02-02 22:40:11 +0100

    Update Terrain2.java

--- Concurrency/Ants/Terrain2.java
+++ Concurrency/Ants/Terrain2.java
@@ -2,7 +2,7 @@ import java.util.concurrent.locks.*;
 /**
  * Write a description of class Terrain2 here.
  * 
- * @author (your name) 
+ * @author CM 
  * @version (a version number or a date)
  */
 public class Terrain2 implements Terrain
